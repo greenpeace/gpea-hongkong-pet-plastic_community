bdcbc3ae16c87a3ad9d4cc67615bb5126bf9cbbd
CK Lam <ckhk0621@gmail.com>
2021-11-06 16:29:49 +0800

    updated

--- src/components/TsuenWanHero/index.js
+++ src/components/TsuenWanHero/index.js
@@ -4,7 +4,7 @@ import "./index.scss";
 
 import Background from "../../assets/images/tsuen-wan/background.svg";
 
-import Logo from "../../assets/images/tsuen-wan/logo.png";
+import Logo from "../../assets/images/tsuen-wan/logo_edit.png";
 
 import Slide1 from "../../assets/images/tsuen-wan/slide_001.jpeg";
 import Slide2 from "../../assets/images/tsuen-wan/slide_002.jpeg";
@@ -12,7 +12,12 @@ import Slide3 from "../../assets/images/tsuen-wan/slide_003.jpeg";
 import Slide4 from "../../assets/images/tsuen-wan/slide_004.jpeg";
 import Slide5 from "../../assets/images/tsuen-wan/slide_005.jpeg";
 
+import CoverSm from "../../assets/images/hero-video/cover-sm.jpg";
+import Mp4 from "../../assets/videos/tsuen-wan/IMG_7761.mp4";
+import Webm from "../../assets/videos/tsuen-wan/IMG_7761.webm";
+
 export default props => {
+  let resetTimeout;
   const { sectionId, devicetype } = props;
   const breakPoints = [
     { width: 1, itemsToShow: 1 }
@@ -23,11 +28,23 @@ export default props => {
 
   const CardData = [
     { id: 1, src: Slide1 },
-    { id: 2, src: Slide2 },
-    { id: 3, src: Slide3 },
-    { id: 4, src: Slide4 },
-    { id: 5, src: Slide5 }
+    { id: 2, src: Slide3 },
+    { id: 3, src: Slide4 },
+    { id: 4, src: Slide5 },
+    { id: 5, src: Slide2 }
   ];
+
+  const carouselRef = React.useRef(null);
+  const onNextStart = (currentItem, nextItem) => {
+    if (currentItem.index === nextItem.index) {
+      carouselRef.current.goTo(0);
+    }
+  };
+  const onPrevStart = (currentItem, nextItem) => {
+    if (currentItem.index === nextItem.index) {
+      carouselRef.current.goTo(CardData.length);
+    }
+  };
 
   return (
     <>
@@ -40,6 +57,10 @@ export default props => {
             <div className="column">
               <div className="twLogoWrap">
                 <img src={Logo} />
+                {/* <video muted autoPlay={true} poster={CoverSm} controls={true}>
+                  <source src={Mp4} type="video/mp4" />
+                  <source src={Webm} type="video/webm" />
+                </video> */}
               </div>
               <div className="tw-main-description-wrap">
                 <div className="tw-main-description">
@@ -56,28 +77,39 @@ export default props => {
                   className="carousel-slice"
                   showArrows={true}
                   pagination={true}
-                  // renderArrow={myArrow}
+                  enableAutoPlay
+                  autoPlaySpeed={5000}
+                  ref={carouselRef}
+                  onPrevStart={onPrevStart}
+                  onNextStart={onNextStart}
+                  onNextEnd={({ index }) => {
+                    clearTimeout(resetTimeout);
+                    if (index + 1 === CardData.length) {
+                      resetTimeout = setTimeout(() => {
+                        carouselRef.current.goTo(0);
+                      }, 5000);
+                    }
+                  }}
+                  disableArrowsOnEnd={false}
                 >
-                  {CardData.slice(0)
-                    .reverse()
-                    .map(card => (
-                      <div key={card.id} className="slice">
-                        <div
-                          className="slice-img-frame"
-                          style={{ backgroundImage: `url(${card.src})` }}
-                        >
-                          {/* <img
+                  {CardData.slice(0).map(card => (
+                    <div key={card.id} className="slice">
+                      <div
+                        className="slice-img-frame"
+                        style={{ backgroundImage: `url(${card.src})` }}
+                      >
+                        {/* <img
                           src={card.character}
                           className="character"
                           alt=""
                         /> */}
-                        </div>
-                        <div className="slice-body">
+                      </div>
+                      {/* <div className="slice-body">
                           <div className="slice-text">試吓講2句</div>
                         </div>
-                        <div className="slice-backdrop"></div>
-                      </div>
-                    ))}
+                        <div className="slice-backdrop"></div> */}
+                    </div>
+                  ))}
                 </Carousel>
               </div>
             </div>
--- src/components/TsuenWanIntro/index.js
+++ src/components/TsuenWanIntro/index.js
@@ -3,6 +3,7 @@ import "./index.scss";
 
 import Logo from "../../assets/images/tsuen-wan/logo_horizontal.png";
 import Background from "../../assets/images/tsuen-wan/background_custom.svg";
+import Step from "../../assets/images/tsuen-wan/step.jpg";
 
 export default props => {
   return (
@@ -16,22 +17,37 @@ export default props => {
         </div>
 
         <div className="tw-intro-wrap">
-          <div>
-            「走塑」不是一個人的事，亦不只得一個方式：
+          <div className="row">
+            <div className="column">
+              <div>
+                <b>「走塑」不是一個人的事，亦不只得一個方式</b>
+                <br />
+                自備餐具、拒用無謂包裝，或以社區網絡推動重用容器，抗衡即棄塑膠，一同建構我們理想的走塑社區。
+                <br />
+                <br />
+              </div>
+              <div>
+                {/* <b>重用餐具借還流程:</b>
             <br />
-            自備餐具、拒用無謂包裝，或以社區網絡推動重用容器，抗衡即棄塑膠，一同建構我們理想的走塑社區。
-          </div>
-          <div>
-            重用餐具借還流程:
+            1.直接於店鋪借重用餐具(無需按金)
             <br />
-            1. 直接於店鋪借重用餐具(無需按金)
+            2.活動時間內放回歸還點
             <br />
-            3. 活動時間內放回歸還點
-            <br />
-            <br />
-            活動日期: 2021年11月20、12月18日 ; 2022年 1月15日
-            <br />
-            活動時間: 中午12時 - 下午5時
+            <br /> */}
+                活動地點: 荃灣路德圍
+                <br />
+                <div style={{ display: "flex" }}>
+                  <div className="dateWrap">活動日期:</div>
+                  <div>2021年11月27、2021年12月18日、2022年 1月15日</div>
+                </div>
+                活動時間: 中午12時 - 下午5時
+              </div>
+            </div>
+            <div className="column">
+              <div className="stepWrap">
+                <img src={Step} />
+              </div>
+            </div>
           </div>
         </div>
       </div>
