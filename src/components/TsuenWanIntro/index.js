ea6ba09ff1c71652644e50ce66a8c23d901c2436
KFC <chris.chan@greenpeace.org>
2022-01-12 15:35:11 +0800

    update: postpone content

--- src/components/TsuenWanIntro/index.js
+++ src/components/TsuenWanIntro/index.js
@@ -20,30 +20,39 @@ export default props => {
           <div className="row">
             <div className="column">
               <div className="tw-intro">
-                <div className="subtitle">
+                <h2 className="subtitle">
                   「走塑」不是一個人的事，亦不只得一個方式
+                </h2>
+                <div>
+                  自備餐具、拒用無謂包裝，或以社區網絡推動重用容器，抗衡即棄塑膠，一同建構我們理想的走塑社區。
                 </div>
-                自備餐具、拒用無謂包裝，或以社區網絡推動重用容器，抗衡即棄塑膠，一同建構我們理想的走塑社區。
                 <br />
+                <div>
+                  <b>重用餐具借還流程:</b>
+                  <br />
+                  1.直接於店鋪借重用餐具(無需按金)
+                  <br />
+                  2.活動時間內放回歸還點
+                  <br />
+                </div>
                 <br />
-              </div>
-              <div>
-                {/* <b>重用餐具借還流程:</b>
-            <br />
-            1.直接於店鋪借重用餐具(無需按金)
-            <br />
-            2.活動時間內放回歸還點
-            <br />
-            <br /> */}
-                活動地點: 荃灣路德圍
-                <br />
-                <div style={{ display: "flex" }}>
-                  <div className="dateWrap">活動日期:</div>
-                  <div>
-                    2021年11月27日、2021年12月11日、2022年 1月15日(星期六)
+                <div>
+                  - 活動地點: 荃灣路德圍
+                  <br />
+                  <div style={{ display: "flex" }}>
+                    <div className="dateWrap">- 活動日期:</div>
+                    <div>
+                      2021年11月27日、2021年12月11日、2022年 1月15日(星期六)
+                    </div>
                   </div>
+                  - 活動時間: 中午12時 - 下午5時
+                  <br />
+                  <b>
+                    <small>
+                      * 因應政府的防疫指引，原定1月15日的活動將延期舉行。
+                    </small>
+                  </b>
                 </div>
-                活動時間: 中午12時 - 下午5時
               </div>
             </div>
             <div className="column">
