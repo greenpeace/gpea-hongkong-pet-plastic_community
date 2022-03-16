c42c17cef4da5502140699aea19acab069aceebd
CK Lam <ckhk0621@gmail.com>
2022-03-16 13:31:39 +0800

    google map interactive

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./index.scss";
 
 export default props => {
+  const [mapClass, setMapClass] = useState("disablePointer");
   return (
     <div className="hk-map-wrap">
-      <div className="map-responsive">
-        <iframe src="https://www.google.com/maps/d/embed?mid=1FWC3p6FNTkr_fzqkowE4kizZy28mGV2x&ehbc=2E312F"></iframe>
+      <div
+        className="map-responsive"
+        onClick={() =>
+          setMapClass(
+            mapClass === "disablePointer" ? "activePointer" : "disablePointer"
+          )
+        }
+        onMouseLeave={() => {
+          setMapClass("disablePointer");
+        }}
+      >
+        <iframe
+          className={mapClass}
+          src="https://www.google.com/maps/d/embed?mid=1FWC3p6FNTkr_fzqkowE4kizZy28mGV2x&ehbc=2E312F"
+        ></iframe>
       </div>
     </div>
   );
